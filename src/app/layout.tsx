e9b14c6600212c87540860e30e93faa30e9e9ae5
James Wallis <j@wallis.dev>
2023-09-12 23:03:33 +0100

    feat: add favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/paintx/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/paintx/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/paintx/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/paintx/site.webmanifest" />
+      </head>
       <body className={roboto.className}>
         <Navigation />
         {children}
